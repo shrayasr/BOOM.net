343fa08dc24690b3d4218f05220409547ef97d04
Shrayas <shrayasr@gmail.com>
2015-11-16 20:24:10 +0530

    Change assembly names to itko

--- BOOM.net/Properties/AssemblyInfo.cs
+++ BOOM.net/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BOOM.net")]
+[assembly: AssemblyTitle("itko")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BOOM.net")]
+[assembly: AssemblyProduct("itko")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
